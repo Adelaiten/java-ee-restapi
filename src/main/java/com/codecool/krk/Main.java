2c98d998e08bbcc1549851591cabe22b0c4084f3
Adelaiten <karoltrzaska19@gmail.com>
2019-01-15 16:11:56 +0100

    hibernate objects working

---
+++ src/main/java/com/codecool/krk/Main.java
@@ -0,0 +1,59 @@
+package com.codecool.krk;
+
+import com.codecool.krk.models.Comment;
+import com.codecool.krk.models.Note;
+import com.codecool.krk.models.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Main {
+    public static void populateDb(EntityManager em) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date birthDate1 = Calendar.getInstance().getTime();
+        try {
+            birthDate1 = sdf.parse("1997-07-21");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        User user = new User();
+        user.setId(1);
+        user.setName("karol");
+        user.setNick("notarian");
+        user.setSurname("trzaska");
+        user.setEmail("karoltrzaska19@gmail.com");
+
+        Comment comment = new Comment();
+        comment.setId(1);
+        comment.setContent("hahaha");
+        comment.setUser(user);
+        comment.setDate(birthDate1);
+        Note note = new Note();
+        note.setContent("hehehe");
+        note.setDate(birthDate1);
+        note.setNoteId(1);
+        note.setTitle("Title");
+        note.setUser(user);
+        comment.setNote(note);
+    }
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("szkolna_17");
+        EntityManager em = emf.createEntityManager();
+
+        populateDb(em);
+        em.clear(); //clear hibernate cache - force next statements to read data from db
+
+        em.close();
+        emf.close();
+
+    }
+}
---
+++ src/main/java/com/codecool/krk/models/User.java
@@ -0,0 +1,93 @@
+package com.codecool.krk.models;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity(name = "Users")
+public class User {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String nick;
+    private String name;
+    private String surname;
+    private String email;
+
+
+    @OneToMany(cascade={CascadeType.ALL}, mappedBy = "user")
+    @ElementCollection
+    private List<Note> notes;
+
+    public User() {
+
+    }
+
+    public User(int id, String nick, String name, String surname, String email, List<Note> notes) {
+        this.id = id;
+        this.nick = nick;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.notes = notes;
+    }
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+
+    @OneToMany(mappedBy = "user")
+
+    @ElementCollection
+    private List<Comment> comments;
+
+    public List<Note> getNotes() {
+        return notes;
+    }
+
+    public void setNotes(List<Note> notes) {
+        this.notes = notes;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
