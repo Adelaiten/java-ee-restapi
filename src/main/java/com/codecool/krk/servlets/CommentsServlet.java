b8dcf0b851d7a80dc777047a70f8dc0d078e246c
Paprok <rafal.becla@gmail.com>
2019-01-16 14:15:44 +0100

    fuck the pom

---
+++ src/main/java/com/codecool/krk/servlets/CommentsServlet.java
@@ -0,0 +1,7 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns = {"/comments/*"})
+public class CommentsServlet extends HttpServlet {
+    
+}
