18b72fa865d7005159bca8e6dc7e418be1a7fbc4
Mateusz <kaspazza@gmail.com>
2019-01-17 10:33:21 +0100

    create posibility to get all notes or one note

--- src/main/java/com/codecool/krk/servlets/NotesServlet.java
+++ src/main/java/com/codecool/krk/servlets/NotesServlet.java
@@ -14,18 +14,29 @@ import java.io.IOException;
 public class NotesServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("szkolna_17");
-        String string = new String();
+        String urlString = request.getRequestURI();
+        urlString = urlString.replace("notes", "");
+        urlString = urlString.replace("/", "");
 
-        EntityManager em = emf.createEntityManager();
 
-        Note note = em.find(Note.class, 1);
-        response.getWriter().write("<html><body>" +
-                "Id: " + note.getNoteId() + "<br>" +
-                "Author: " + note.getUser().getName() + "<br>" +
-                "Title: " + note.getTitle() + "<br>" +
-                "Date: " + note.getDate() + "<br>" +
-                "Content: " + note.getContent() + "<br>" +
-                "</body></html>");
-    }
+        if(request.getRequestURI().isEmpty()){
+            response.getWriter().write("<html><body>ALL NOTES</body></html>");
+        } else if(request.getRequestURI().matches("/notes/")) {
+            EntityManagerFactory emf = Persistence.createEntityManagerFactory("szkolna_17");
+            String string = new String();
+
+            EntityManager em = emf.createEntityManager();
+
+            Note note = em.find(Note.class, urlString);
+            response.getWriter().write("<html><body>" +
+                    "Id: " + note.getNoteId() + "<br>" +
+                    "Author: " + note.getUser().getName() + "<br>" +
+                    "Title: " + note.getTitle() + "<br>" +
+                    "Date: " + note.getDate() + "<br>" +
+                    "Content: " + note.getContent() + "<br>" +
+                    "THE URL IS: " + request.getRequestURI() + "<br>" +
+                    "</body></html>");
+        }
+        }
+
 }
